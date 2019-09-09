1c9afe65d251242d73a61d7bd56722ed340ab49e
Pjmcnally <Patrick.J.McNally@gmail.com>
2019-09-08 22:29:29 -0500

    Finish terminal hacker.

--- Projects/02_TerminalHacker/Assets/Hacker.cs
+++ Projects/02_TerminalHacker/Assets/Hacker.cs
@@ -6,13 +6,13 @@ using UnityEngine;
 public class Level
 {
     public string Name;
-    public int IntValue;
-    public string strValue;
+    public string WinPhrase;
     public string[] Passwords;
 
-    public Level (string name, string[] passwords)
+    public Level (string name, string winPhrase, string[] passwords)
     {
         Name = name;
+        WinPhrase = winPhrase;
         Passwords = passwords;
     }
 }
@@ -22,9 +22,9 @@ public class Hacker : MonoBehaviour
     // Game configuration data
     private readonly Level[] Levels =
     {
-        new Level("Wikipedia", new string[] { "history", "science", "law", "math", "music" }),
-        new Level("Google", new string[] { "gmail", "drive", "calendar", "youtube", "translate", "search" }),
-        new Level("Path of Exile", new string[] { "slayer", "gladiator", "champion", "assassin", "saboteur", "trickster", "juggernaut", "berserker", "chieftain", "necromancer", "occultist", "elementalist", "deadeye", "raider", "pathfinder", "inquisitor", "hierophant", "guardian", "ascendant" })
+        new Level("Wikipedia", "Better get what you want quickly. This page will be probably be locked soon...", new string[] { "history", "science", "law", "math", "music" }),
+        new Level("Google", "Google is always watching. Hopefully\nthey didn't notice...", new string[] { "gmail", "drive", "calendar", "youtube", "translate", "search" }),
+        new Level("Path of Exile", "Run fast exile. Death is always lurkingjust behind you...", new string[] { "slayer", "gladiator", "champion", "assassin", "saboteur", "trickster", "juggernaut", "berserker", "chieftain", "necromancer", "occultist", "elementalist", "deadeye", "raider", "pathfinder", "inquisitor", "hierophant", "guardian", "ascendant" })
     };
 
     enum Screen
@@ -38,6 +38,9 @@ public class Hacker : MonoBehaviour
     Screen CurrentScreen;
     Level CurrentLevel;
     string CurrentPassword;
+    const int StartingGuesses = 3;
+    int GuessesRemaining;
+    const string MenuHint = "Type menu to return to the menu.";
 
     // Start is called before the first frame update
     void Start()
@@ -60,7 +63,10 @@ public class Hacker : MonoBehaviour
 
     void OnUserInput(string input)
     {
-        if (input.ToLower() == "menu")  // We can always go to main menu
+        StopAllCoroutines();
+        if (
+            input.ToLower() == "menu" || // We can always go to main menu
+            CurrentScreen == Screen.Win)  // Any input on win screen takes us back to main menu
         {
             ShowMainMenu();
         }
@@ -79,33 +85,76 @@ public class Hacker : MonoBehaviour
         if (int.TryParse(input, out int index) && Levels.ElementAtOrDefault(index) != null)
         {
             CurrentLevel = Levels[index];
-            StartGame();
+            SetRandomPassword();
+            AskForPassword();
         }
         else
         {
-            Terminal.WriteLine("Please enter a valid input:");
+            ShowMainMenu();
         }
     }
 
-    void StartGame()
+    void AskForPassword()
     {
         CurrentScreen = Screen.Password;
+        Terminal.ClearScreen();
+        Terminal.WriteLine($"{MenuHint}\n");
+        Terminal.WriteLine("Enter your password: ");
+        Terminal.WriteLine($"--> Hint: {CurrentPassword.Anagram()}");
+        Terminal.WriteLine($"--> Guesses Remaining: {GuessesRemaining}");
+        
+    }
+
+    void SetRandomPassword()
+    {
+        GuessesRemaining = StartingGuesses;
         int randInt = Random.Range(0, CurrentLevel.Passwords.Length);
         CurrentPassword = CurrentLevel.Passwords[randInt];
-
-        Terminal.ClearScreen();
-        Terminal.WriteLine("Please enter your password: ");
     }
 
     void CheckPassword(string input)
     {
+        GuessesRemaining -= 1;
+
         if (input.ToLower() == CurrentPassword)
         {
-            Terminal.WriteLine("Congratulations!");
+            DisplayWinScreen();
         } 
-        else
+        else if (GuessesRemaining > 0)
         {
-            Terminal.WriteLine("Please enter another password.");
+            AskForPassword();
+        }
+        else 
+        {
+            SetRandomPassword();
+            AskForPassword();
+        }
+    }
+
+    void DisplayWinScreen()
+    {
+        CurrentScreen = Screen.Win;
+        Terminal.ClearScreen();
+        ShowLevelReward();
+    }
+
+    void ShowLevelReward()
+    {
+        Terminal.WriteLine($"You hacked into {CurrentLevel.Name}!");
+        Terminal.WriteLine("");
+        Terminal.WriteLine(CurrentLevel.WinPhrase);
+        StartCoroutine(WriteMatrixText());
+    }
+
+    IEnumerator WriteMatrixText()
+    {
+        yield return new WaitForSeconds(5);
+        Terminal.ClearScreen();
+        while (true)
+        {
+            System.Guid g = System.Guid.NewGuid();
+            Terminal.WriteLine(g.ToString());
+            yield return new WaitForSeconds(.1f);
         }
     }
 
